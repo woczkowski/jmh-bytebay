20494074e1bc3378303c3721312b2ccd7abd645d
woczkowski <woczkowski@itkontekst.pl>
2018-03-14 16:04:16 +0100

    adding integers

--- src/main/java/pl/itkontekst/jmh/bytebay/MyBenchmark.java
+++ src/main/java/pl/itkontekst/jmh/bytebay/MyBenchmark.java
@@ -1,11 +1,37 @@
 package pl.itkontekst.jmh.bytebay;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.*;
 
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@Fork(1)
+@State(Scope.Benchmark)
 public class MyBenchmark {
 
+    private int val1 = 1;
+    private int val2 = 1;
+
     @Benchmark
-    public void testMethod() {
+    public int baseline() {
+        return 2;
     }
 
+    @Benchmark
+    public void addWrong(){
+        int i = 1+1;
+    }
+
+    @Benchmark
+    public int add(){
+        return 1+1;
+    }
+
+    @Benchmark
+    public int addState(MyBenchmark state){
+        return state.val1+state.val2;
+    }
 }
