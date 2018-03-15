324300545b50b597c568712222465da67bbe800e
woczkowski <woczkowski@itkontekst.pl>
2018-03-15 14:10:10 +0100

    false cache sharing

--- src/main/java/pl/itkontekst/jmh/bytebay/MyBenchmark.java
+++ src/main/java/pl/itkontekst/jmh/bytebay/MyBenchmark.java
@@ -4,19 +4,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Group;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Threads;
-import org.openjdk.jmh.annotations.Warmup;
+
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -25,46 +17,55 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @Fork(1)
 @Warmup(iterations = 10)
 @Measurement(iterations = 10)
-@State(Scope.Benchmark)
-@Threads(10)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class MyBenchmark {
 
-    private int[] integers;
-    
-    @Setup(Level.Iteration)
-    public void init(){
-        integers = new Random().ints(10000000).toArray();
+    @State(Scope.Group)
+    public static class Shared {
+        private int readField;
+        private int writeField;
     }
-
-    
-    @Benchmark
-    @Group("sum")
-    public int sumUp() {
-        int sum = 0;
-        for (int integer : integers) {
-            sum+=integer;
-        }
-        return sum;
+    @State(Scope.Group)
+    public static class Read {
+        private int readField;
+    }
+    @State(Scope.Group)
+    public static class Write {
+        private int writeField;
     }
 
     @Benchmark
-    @Group("sum")
-    public int sumDown() {
-        int sum = 0;
-        for (int i = integers.length -1; i >= 0; i--) {
-            sum += integers[i];
-        }
-        return sum;
+    @Group("shared")
+    public int readerShared(Shared shared) {
+        return shared.readField;
     }
-    
-    
+
+    @Benchmark
+    @Group("shared")
+    public void writerShared(Shared shared) {
+        shared.writeField++;
+    }
+
+    @Benchmark
+    @Group("padded")
+    public int readerPadded(Read read) {
+        return read.readField;
+    }
+
+    @Benchmark
+    @Group("padded")
+    public void writerPadded(Write write) {
+        write.writeField++;
+    }
+
+
     public MyBenchmark() {
     }
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
-                .threads(Runtime.getRuntime().availableProcessors() * 16)
-                .syncIterations(false)
+                .threads(Runtime.getRuntime().availableProcessors())
                 .build();
         new Runner(opt).run();
     }
